cb1903550fb82714871e75bebe583d87ed0b9e28
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2023-01-15 14:09:24 -0800

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import Header from "./components/Header";
+import NavBar from "./components/Header";
 
 import Portfolio from "./components/main/Main";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <NavBar></NavBar>
       <Portfolio></Portfolio>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Header() {
-  return <div>Header</div>;
-}
+import "./Header.css";
+
+const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <header>
+      <nav>
+        <div className="left-nav">
+          <Link to="/">Zach Sultan</Link>
+        </div>
+        <div className="right-nav">
+          <button className="dropdown" onClick={() => setOpen(!isOpen)}>
+            <i className="fa fa-bars"></i>Menu
+          </button>
+          {isOpen && (
+            <ul>
+              <li className="about-button">
+                <Link to="/about">About Zach</Link>
+              </li>
+              <li className="contact-button">
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li className="projects-button">
+                <Link to="/about">Projects</Link>
+              </li>
+              <li className="blog-button">
+                <Link to="/blog">Blog</Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
