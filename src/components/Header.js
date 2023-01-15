ad53de14b96e289aef263a497c79762fabd4a65b
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2023-01-15 13:37:32 -0800

    header bomb

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
-const Header = () => {
-  const [isOpen, setOpen] = useState(false);
-
-  return (
-    <header>
-      <nav>
-        <div className="left-nav">
-          <Link to="/">Zach Sultan</Link>
-          <Link to="/about">About Zach</Link>
-        </div>
-        <div className="right-nav">
-          <button onClick={() => setOpen(!isOpen)}>
-            <i className="fa fa-bars"></i>
-          </button>
-          {isOpen && (
-            <ul>
-              <li>
-                <Link to="/about">About Zach</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-              <li>
-                <Link to="/about">Projects</Link>
-              </li>
-              <li>
-                <Link to="/blog">Blog</Link>
-              </li>
-            </ul>
-          )}
-        </div>
-      </nav>
-    </header>
-  );
-};
-
-export default Header;
+export default function Header() {
+  return <div>Header</div>;
+}
